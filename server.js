2191d540cd158325d53d650dab1bccf84c2efd1a
myungho <skqoaudgh2@hanmail.net>
2019-07-10 15:37:49 +0900

    Initial Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.use('/', (req, res, next) => {
+    res.render('index.ejs');
+});
+
+app.listen(3000, () => {
+    console.log('express server is opened on port 3000.');
+})
